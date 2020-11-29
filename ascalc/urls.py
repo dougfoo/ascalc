0fe5c24735f76d9e78724dcbb38e198848e3ff13
dougfoo <doug.cha@gmail.com>
2020-11-29 12:00:54 +0900

    added react nodes

--- ascalc/urls.py
+++ ascalc/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('calcapp/', include('calcapp.urls')),
     path('admin/', admin.site.urls),
+    path('', include('frontend.urls')),
 ]
